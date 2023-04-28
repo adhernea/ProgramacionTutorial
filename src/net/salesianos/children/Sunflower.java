fb271712ef832a7d508c6cb0532ba50d3ebea549
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-04-28 13:50:44 +0100

    feat: added Sunflower class which inherit from Flower

---
+++ src/net/salesianos/children/Sunflower.java
@@ -0,0 +1,18 @@
+package net.salesianos.children;
+
+import net.salesianos.parent.Flower;
+
+public class Sunflower extends Flower {
+
+    public Sunflower(int petals) {
+        super(petals, "yellow");
+        // System.out.println(this.BEST_SEASON);
+    }
+
+    public void followSun() { }
+    public void openUp() { }
+    public void closeDown() { }
+
+}
+
+
