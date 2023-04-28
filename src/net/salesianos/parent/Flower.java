942e182802ed33e55e569343e063de6b92fceae5
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-04-28 13:50:20 +0100

    feat(Flower): override equals

--- src/net/salesianos/parent/Flower.java
+++ src/net/salesianos/parent/Flower.java
@@ -35,4 +35,31 @@ public class Flower {
         return "{ petalsNumber:" + petalsNumber + ", color," + color + ", BEST_SEASON," + BEST_SEASON + "}";
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        // Si el objeto es el LITERALMENTE el mismo en memoria, retorna verdadero.
+        if (obj == this) {
+            return true;
+        }
+
+        // Si el objeto es de otra clase, retorna falso
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        // Transformamos el objeto obj a la clase que hemos validado en el paso anterior
+        // para poder acceder a sus getters y setters
+        Flower flowerObject = (Flower) obj;
+
+        // Comprobamos que los atributos sean iguales, y si lo son, retorna verdadero
+        boolean hasSameColor = this.color == flowerObject.getColor();
+        boolean hasSamePetalsNumber = this.petalsNumber == flowerObject.getPetalsNumber();
+        if (hasSameColor && hasSamePetalsNumber) {
+            return true;
+        }
+
+        // Si no se cumple ninguna de las condiciones anteriores, retorna falso
+        return false;
+    }
+
 }
