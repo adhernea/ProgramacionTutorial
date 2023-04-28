53b29ab64ba09ac67ec4ab1335aab52330fb1d8c
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-04-28 13:47:57 +0100

    feat: added Flower class

---
+++ src/net/salesianos/parent/Flower.java
@@ -0,0 +1,33 @@
+package net.salesianos.parent;
+
+public class Flower {
+
+    private int petalsNumber;
+    private String color;
+
+    protected final String BEST_SEASON = "Spring";
+
+    public Flower(int petals, String flowerColor) {
+        this.petalsNumber = petals;
+        this.color = flowerColor;
+    }
+
+    protected void flourish() { }
+
+    public int getPetalsNumber() {
+        return petalsNumber;
+    }
+
+    public void setPetalsNumber(int petalsNumber) {
+        this.petalsNumber = petalsNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
